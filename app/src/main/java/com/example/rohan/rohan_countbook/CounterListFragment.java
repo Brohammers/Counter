22966729461ba273bb21aad1ec5bf4c4f82b45fa
Brohammers <rohan@ualberta.ca>
2017-09-06 18:31:54 -0600

    Added list > detail callback, and toolbar

--- app/src/main/java/com/example/rohan/rohan_countbook/CounterListFragment.java
+++ app/src/main/java/com/example/rohan/rohan_countbook/CounterListFragment.java
@@ -1,10 +1,13 @@
 package com.example.rohan.rohan_countbook;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.LayoutInflaterCompat;
 import android.support.v4.widget.TextViewCompat;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +16,7 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,15 +25,50 @@ import java.util.List;
 
 public class CounterListFragment extends Fragment {
 
+    OnCounterCardSelectedListener mCallback;
+
+    public interface OnCounterCardSelectedListener {
+
+        void onCounterSelected(int index);
+
+    }
+
+
+    private RecyclerView mRecyclerView;
+    private CounterAdapter mCounterAdapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.counter_list, container, false);
 
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.recyclerView);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        List<Counter> c =  new ArrayList<Counter>();
+        Counter c1 = new Counter("Buy Eggs", "List of eggs to buy", 10);
+        c.add(c1);
+
+        mCounterAdapter = new CounterAdapter(c);
+        mRecyclerView.setAdapter(mCounterAdapter);
+
+
+
         //do all your view setup here
 
 
         return v;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mCallback = (OnCounterCardSelectedListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement interface");
+        }
     }
 
 
@@ -44,7 +83,7 @@ public class CounterListFragment extends Fragment {
 
         @Override
         public CounterHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-            View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.counter_list, null));
+            View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.counter_card, null);
             CounterHolder counterHolder = new CounterHolder(v);
             return counterHolder;
         }
@@ -52,29 +91,61 @@ public class CounterListFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(CounterHolder counterHolder, int index) {
-            Counter counter = mCounterList.get(index);
+            final Counter counter = mCounterList.get(index);
+            final int INDEX = index;
 
 
 
             counterHolder.mName.setText(counter.getName());
-            counterHolder.mCurrentValue.setText(counter.getCurrentValue());
+            Integer i = counter.getCurrentValue();
+            counterHolder.mCurrentValue.setText(i.toString());
             counterHolder.mDate.setText(counter.getLastModifiedDate());
             counterHolder.mDescription.setText(counter.getComment());
+
 
             counterHolder.mIncrement.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
+
+                    counter.setCurrentValue(counter.getCurrentValue() + 1);
+
+                    updateUI(INDEX);
 
                     //Increment counter
 
                 }
             });
 
+            counterHolder.mDecrement.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
 
+                    counter.setCurrentValue(counter.getCurrentValue() - 1);
 
+                    updateUI(INDEX);
+                    //Decrement counter
+
+                }
+            });
+
+            counterHolder.mDescription.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    mCallback.onCounterSelected(INDEX);
+
+                }
+            });
+
+        }
+
+        private void updateUI (int index) {
+            mCounterAdapter.notifyItemChanged(index);
 
 
         }
+
+
 
         @Override
         public int getItemCount() {
--- app/src/main/java/com/example/rohan/rohan_countbook/MainActivity.java
+++ app/src/main/java/com/example/rohan/rohan_countbook/MainActivity.java
@@ -4,14 +4,21 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CounterListFragment.OnCounterCardSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initializeCounterList();
+
+        Toolbar t = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(t);
     }
 
     private void initializeCounterList() {
@@ -19,5 +26,36 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction ft = fm.beginTransaction();
         ft.add(R.id.frameLayout, new CounterListFragment());
         ft.commit();
+    }
+
+    @Override
+    //IMPLEMENT SHOWING DETAILS VIEW
+    public void onCounterSelected(int index) {
+        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.addCounter:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+
     }
 }
